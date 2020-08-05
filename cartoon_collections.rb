035a467381df05df252daecff8af6e5aa0e408d6
Bria White <bria.awhite@gmail.com>
2020-08-05 23:09:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls_long)
   calls_long.any?{|i| i.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
