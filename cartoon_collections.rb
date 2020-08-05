9786ba837e82295e635c7b8d6f890a06cdf4fcdb
Bria White <bria.awhite@gmail.com>
2020-08-05 23:48:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  cheese_types.include?("cheddar")
+  cheddar_cheese.select do |cheese| 
+    cheese.include?
 end
