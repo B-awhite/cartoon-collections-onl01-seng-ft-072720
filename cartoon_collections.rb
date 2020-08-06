1d577a3c9e83259c48e3118933d10410e8a3a219
Bria White <bria.awhite@gmail.com>
2020-08-06 00:00:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
-  cheddar_cheese.find{|i| i.include?(cheddar)}
+  cheddar_cheese.find{|i| i.include?("cheddar")}
   cheese_types = ["cheddar", "gouda", "camembert"]
    
 end
